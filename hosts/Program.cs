124c55267ae9f94027aa94beafeeaaac72e288b6
Jebibot <83044352+jebibot@users.noreply.github.com>
2022-12-22 21:07:20 +0900

    feat: initial commit

---
+++ hosts/Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace hosts
+{
+    internal class Program
+    {
+        private const string COMMENT_AFLINK = "# AFLINK";
+
+        static void Main(string[] args)
+        {
+            // Get the hosts file path from the registry
+            // Windows 7+ does not use this key, but if modified, they probably knew what they're doing
+            RegistryKey regKey = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\");
+            string hostsFile = null;
+            if (regKey != null)
+            {
+                hostsFile = (string)regKey.GetValue("DataBasePath");
+                regKey.Close();
+            }
+            if (hostsFile == null || hostsFile.Trim() == string.Empty)
+            {
+                hostsFile = Environment.ExpandEnvironmentVariables(@"%SystemRoot%\System32\drivers\etc");
+            }
+            hostsFile += @"\hosts";
+
+            bool modified = args.Length > 0; // whether new entries exist
+            List<string> result = new List<string>();
+            if (File.Exists(hostsFile))
+            {
+                // Make the hosts file writeable
+                FileAttributes attributes = File.GetAttributes(hostsFile);
+                if ((attributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
+                {
+                    File.SetAttributes(hostsFile, attributes & ~FileAttributes.ReadOnly);
+                }
+
+                foreach (var line in File.ReadLines(hostsFile))
+                {
+                    // Remove previous entries
+                    if (line.Contains(COMMENT_AFLINK))
+                    {
+                        modified = true;
+                        continue;
+                    }
+                    result.Add(line);
+                }
+            }
+            if (!modified)
+            {
+                return;
+            }
+
+            // Add new entries
+            foreach (var host in args)
+            {
+                if (Uri.CheckHostName(host) != UriHostNameType.Dns)
+                {
+                    throw new Exception("Invalid host!");
+                }
+                result.Add($"127.0.0.1 {host} {COMMENT_AFLINK}");
+            }
+            File.WriteAllLines(hostsFile, result.ToArray());
+        }
+    }
+}
